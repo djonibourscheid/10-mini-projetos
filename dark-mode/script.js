494a300f42721ae2727520aa679a144df31a636e
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-26 23:11:57 -0300

    Back to menu

--- dark-mode/script.js
+++ dark-mode/script.js
@@ -1,5 +1,11 @@
 const button = document.getElementById("toggle")
+const iconMenu = document.querySelectorAll("#back-to-menu-container a img")
 
 button.addEventListener("click", () => {
   document.body.classList.toggle("dark")
+
+  // Back to Menu
+  iconMenu.forEach(e => {
+    e.classList.toggle('hide');
+  });
 })
