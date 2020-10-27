ca4a5cc91421b5c7db4e4453458bb0cf3cf0c490
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-27 10:16:08 -0300

    update hamburger menu

--- hamburger/script.js
+++ hamburger/script.js
@@ -1,6 +1,5 @@
 const btn = document.getElementById("btn")
 const nav = document.getElementById("nav")
-const navWidth = nav.clientWidth
 
 btn.addEventListener("click", () => {
   nav.classList.toggle("open")
