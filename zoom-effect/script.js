dea3f660fb97cbeb8017cdeed16ed515a73727d1
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-27 15:30:42 -0300

    Zoom effect - mobile

--- zoom-effect/script.js
+++ zoom-effect/script.js
@@ -1,16 +1,36 @@
 const container = document.getElementById("container");
 const img = document.querySelector('img');
 
+// PC
 container.addEventListener("mousemove", (e) => {
   const x = e.clientX - e.target.offsetLeft;
   const y = e.clientY - e.target.offsetTop;
 
-  img.style.transformOrigin = `${x}px ${y}px`;
-  img.style.transform = "scale(2)";
+  zoom(x, y);
 })
 
-container.addEventListener("mouseleave", () => {
-  img.style.transformOrigin = `center center`;
-  img.style.transform = "scale(1)";
+container.addEventListener("mouseleave", returnToCenter);
+
+
+// Mobile
+container.addEventListener("touchmove", (e) => {
+  const x = e.touches[0].clientX; - e.target.offsetLeft;
+  const y = e.touches[0].clientY; - e.target.offsetTop;
+
+  zoom(x, y);
 });
 
+container.addEventListener("touchend", returnToCenter);
+
+
+// Functions
+function zoom(x, y) {
+  img.style.transformOrigin = `${x}px ${y}px`;
+  img.style.transform = "scale(2)";
+}
+
+function returnToCenter() {
+  console.log("A")
+  img.style.transformOrigin = `center center`;
+  img.style.transform = "scale(1)";
+}
