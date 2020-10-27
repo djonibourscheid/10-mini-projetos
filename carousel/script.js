14deb7c0755ce45e1d91823ede686b355e750e7a
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-27 14:34:51 -0300

    Responsive

--- carousel/script.js
+++ carousel/script.js
@@ -1,12 +1,17 @@
 const images = document.querySelector(".images");
 let counter = 0;
 
+let widthImage = document.querySelector(".images img").width;
+window.addEventListener("resize", () => {
+  widthImage = document.querySelector(".images img").width;
+})
+
 // Alternate image
 function alternateImage() {
   counter++;
   if (counter == images.children.length) {counter = 0;}
 
-  images.style.transform = `translateX(${-counter * 850}px)`;
+  images.style.transform = `translateX(${-counter * widthImage}px)`;
 }
 
 setInterval(alternateImage, 3500);
