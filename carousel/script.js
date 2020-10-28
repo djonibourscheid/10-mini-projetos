a9fa6ed820dd774907da738161ef122f58cd27e3
Djoni Bourscheid <djonibourscheid00@gmail.com>
2020-10-27 23:41:17 -0300

    carousel correction transition

--- carousel/script.js
+++ carousel/script.js
@@ -1,17 +1,22 @@
-const images = document.querySelector(".images");
-let counter = 0;
+window.addEventListener("load", () => {
+  const images = document.querySelector(".images");
+  let counter = 0;
+  
+  let widthImage = document.querySelector(".images img").width;
+  window.addEventListener("resize", () => {
+    widthImage = document.querySelector(".images img").width;
+  })
+  
+  // Alternate image
+  function alternateImage() {
+    counter++;
+    if (counter == images.children.length) {counter = 0;}
+  
+    images.style.transform = `translateX(${-counter * widthImage}px)`;
+  }
+  
+  setInterval(alternateImage, 3500);
 
-let widthImage = document.querySelector(".images img").width;
-window.addEventListener("resize", () => {
-  widthImage = document.querySelector(".images img").width;
-})
-
-// Alternate image
-function alternateImage() {
-  counter++;
-  if (counter == images.children.length) {counter = 0;}
-
-  images.style.transform = `translateX(${-counter * widthImage}px)`;
-}
-
-setInterval(alternateImage, 3500);
+  const progressBar = document.querySelector(".progress-bar");
+  progressBar.style.animation = "grow 3.5s linear infinite";
+});
